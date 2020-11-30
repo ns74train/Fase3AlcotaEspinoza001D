c2502f0b5c6528b7f4fa2d05add8536bb559be3d
ns74train <benjaminalejandro60@gmail.com>
2020-11-29 23:39:53 -0400

    Incorporando API y Requerimientos Admin y Usuario

--- Libertadores/Libertadores/settings.py
+++ Libertadores/Libertadores/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'copa.apps.CopaConfig',
     'sesion.apps.SesionConfig',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Libertadores/Libertadores/urls.py
+++ Libertadores/Libertadores/urls.py
@@ -18,3 +18,20 @@ urlpatterns = [
 
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
+from django.urls import include, path
+from rest_framework import routers
+from quickstart import views
+
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns += [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
